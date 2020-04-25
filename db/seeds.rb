1e582da2146b2e87a30d47fc8dea25213aece41a
Sandro Soncini <sandrosoncini@hotmail.com>
2020-04-24 21:47:27 -0700

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Post.delete_all
+
+100.times do
+    Post.create(
+        title: Faker::Book.title,
+        body: Faker::ChuckNorris.fact
+    )
+end
+
+post = Post.all
+
+puts Cowsay.say("Generated #{post.count} posts", :cow)
