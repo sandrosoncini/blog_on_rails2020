cabc39771d37dc4f2c87de8d7217e28f2593dd33
Sandro Soncini <sandrosoncini@hotmail.com>
2020-04-24 23:45:57 -0700

    Added contoller destroy, edit and update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
     def index
-        @posts = Post.all
+        @posts = Post.all.order(created_at: :desc )
     end 
 
     def show
@@ -11,5 +11,31 @@ class PostsController < ApplicationController
         @post = Post.new
     end
 
-    
+    def create
+        @post = Post.new(params.require(:post).permit(:title, :body, :image )) 
+        if @post.save  
+            redirect_to posts_path  
+        else 
+            render :new 
+	    end
+    end 
+
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+        redirect_to posts_path
+    end
+
+    def edit 
+        @post = Post.find(params[:id])
+    end 
+
+    def update 
+        @post = Post.find(params[:id])
+        if @post.update(params.require(:post).permit(:title, :body, :image))
+            redirect_to post_path(@post) 
+        else 
+            render :edit
+	    end
+    end
 end
